b1f64362d0881b82be44957936ffd79d3ce321a6
siddharth <siddarthwajge.com>
2026-06-19 16:25:40 +0530

    Connect frontend to Railway backend

--- backend/main.py
+++ backend/main.py
@@ -37,8 +37,7 @@ app = FastAPI(
 # CORS — lets your frontend talk to this backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://bgm8.github.io/cybershield/"
-                   "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
